14dd2084aeb0d2034ff9b8b6c69ac4673a1d9b37
Henrik Josefsson <josefssonhenrik@hotmail.com>
2017-11-17 15:19:19 +0100

    improved gridfinding

--- find_grid2.cpp
+++ find_grid2.cpp
@@ -9,27 +9,66 @@
 using namespace std;
 using namespace cv;
 
+using map<int, vector<int> > = mapiv;
 
 //Take a function as input to get width or heigth
-map<int, int> overlapping(map<int, int>& tree, vector<Rect>& boundingBoxes, ){
+mapiv overlapping(map<int, int>& tree, vector<Rect>& boundingBoxes, function<int(Rect)> val){
   map<int, int> overlap;
   int left = right = tree.begin().first;
+  vector<int> a0;
   for (auto p : tree){
     int ll = p.first;
-    int lr = p.first + boundingBoxes[p.second].width;
+    int lr = p.first + val(boundingBoxes[p.second]);
     if (ll <= right && lr >= left){
       left = min(left, ll);
       right = max(right, lr);
+      a0.push_back(p.second);
     }else{
-      overlap.insert({left, right});
+      sort(a0.begin(), a0.end())
+      overlap.insert({left, a0});
+      a0.clear();
       left = ll;
       right = lr;
     }
   }
+  if (overlap.lower_bound(left) == overlap.end()){
+    sort(a0.begin(), a0.end());
+    overap.insert({left, a0});
+  }
   return overlap;
 }
 
-vector<vector<string> > find_grid(const vector<Rect>& boundingBoxes,const vector<string>& text){
+vector<Rect> split(mapiv& cols,mapiv& rows, vector<Rect> boundingBoxes){
+  for (int i = 0; i < boundingBoxes.size(); i++){
+    Rect r = boundingBoxes[i];
+    Point tl = r.tl();
+    Point br = r.br();
+    auto itlow_y = rows.lower_bound(br.y+1);
+    auto itlow_x = floor_it(cols, tl.x);
+    vector<int> intersect;
+    set_intersection(itlow_y->second.begin(), itlow_y->second.end(),
+		     itlow_x->second.begin(), itlow_x->second.end(), intersect.begin());
+    vector<int> overlap_index;
+    for (int j= 0; j < intersect.size(); j++){      
+      if (compareRect(r, boundingBox[intersect[j]])){
+	overlap_index.push_back(intersect[j]);
+      }
+    }
+    if (overlap_index.size() > 1){
+      Rect a0 = boundingBoxes[overlap_index[0]];
+      boundingBoxes[i] = Rect(a0.x, a0.width,r.y,r.height);
+      for (int j = 1; j < overlap_index.size(); j++){
+	a0 = boundingBoxes[overlap_index[j]];	
+	boundingBoxes.push_back(Rect(a0.x, a0.width, r.y, r.height);
+      }
+    }
+  }
+  return boundingBoxes;
+}
+  
+vector<vector<string> > find_grid(const vector<Rect>& boundingBoxes,
+				  const vector<string>& text)
+{
   map<int, int> xtree;
   map<int, int> ytree;
   for (int i = 0; i < boundingBoxes.size(); i++){
@@ -37,10 +76,12 @@ vector<vector<string> > find_grid(const vector<Rect>& boundingBoxes,const vector
     xtree.insert({r.x, i});
     ytree.insert({r.y, i});
   }
-  map<int, int> cols = overlapping(xtree, boundingBoxes);
-  map<int, int> rows = overlapping(ytree, boundingBoxes);
-  split(cols, rows, boundingBoxes);
-  vector<vector<string> > table(cols.size(), vector<string>(rows.size(),""));
+  mapiv cols = overlapping(xtree, boundingBoxes, [](const Rect& r){return r.width;});
+  mapiv rows = overlapping(ytree, boundingBoxes, [](const Rect& r){return r.height;});  
+  split(rows, cols, boundingsBoxes)
+  vector<vector<string> > table(cols.size(), vector<string>(rows.size(),""));  
+  //TODO
+  //Need rewriting. Will not work (the way it's expected)
   for (int i = 0; i < text.size(); i++){
     Rect r = boundingBoxes[i];
     auto itx = cols.lower_bound(r.x);

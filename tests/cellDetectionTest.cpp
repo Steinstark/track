d003ae50a69005999bbbfcc3265c40ea1eea60c4
Henrik Josefsson <josefssonhenrik@hotmail.com>
2017-11-29 14:34:30 +0100

    implemented celltest

--- tests/cellDetectionTest.cpp
+++ tests/cellDetectionTest.cpp
@@ -4,24 +4,28 @@
 #include "test_engine.hpp"
 #include "detect_cell.hpp"
 
-
 using namespace std;
 using namespace cv;
-//TODO
-//implement GT
-//implement getGroundTrut1
+
+using vvr = vector<vector<Rect> >;
 
 int main(int argc, char** argv){
   vector<string> files = files_in_folder(string(argv[1]));
-  Stats global;
   for (string file : files){
-    GT gt = getGroundTruth(file);
-    for (auto p : gt){      
-      vector<Rect> m = detect_cells(file, p.first);
-      Stats local = compare(m, p.second);
-      global += local;
+    vvr tables, cells;
+    vector<string> text;
+    getGroundTruth(file, tables, cells, text);
+    save(file+pdf, file+png);
+    int index = 0;
+    for (int i = 0; i < tables.size(); i++){
+      for (int j = 0; j < tables[i].size(); j++){
+	stringstream ss;
+	ss << file << "_" << i << png;
+	vector<Rect> d = detect_cells(ss.str(),tables[i][j]);
+	cout << compare(cells[index], d) << endl;
+	index++;
+      }
     }
   }
-  cout << global << endl;
   return 0;
 }

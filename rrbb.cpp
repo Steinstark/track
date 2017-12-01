905ad86f301aaae8ba51a36ae11ae43f6a163a0c
Henrik Josefsson <josefssonhenrik@hotmail.com>
2017-12-01 15:53:37 +0100

    added homogenity

--- rrbb.cpp
+++ rrbb.cpp
@@ -40,36 +40,85 @@ void getOverlapCount(const vector<Rect>& bb,
   }
 }
 
+//TODO
+//implement
 void recursiveFilter(Rect r){
-  
+  return;
 }
-/*
-bool isHomogenous(Stats stats){
-  
-}
-*/
 
-void homogenityStats(){
-  Mat src;
-  Mat dst;
-  int s = 5;
-  reduce(src, dst, 1, CV_REDUCE_SUM);
-  blur(src, dst, Size(1,5));
-  
+bool isExtrema(double a, double b){
+  return (a < 0 && b > 0) || (a > 0 && b < 0);
 }
-    
-vector<Rect> homogenitySplit(Rect r){
-  vector<Rect> ret;
-  //auto stats = homogenityStats(r);
-  //  if (isHomogenous(stats))    
-  //  return r;
-  //  vector<Rect>  srv = split(r, stats);
-  //vector<Rect> v0 = homogenitySplit(srv[0]);
-  //vector<Rect> v1 = homogenitySplit(srv[1]);
-  //vector<Rect>  ret;
-  //ret.insert(v0.begin(), v0.end());
-  //ret.insert(v1.begin(), v1.end());
-  return ret;
+
+double homogenity_stats(Mat& bw, Mat& sob, Mat& delta){
+  Mat red;
+  int s = 5;
+  //histogram
+  int scale = 255;
+  reduce(bw, red, 1, CV_REDUCE_SUM, CV_64F);
+  Mat sred = red / scale;  
+  Mat blurred;
+  //medelkernel 
+  blur(sred, blurred, Size(1,30));  
+  //gradienten
+  Scharr(blurred, sob, -1, 0, 1);
+  vector<int> a0;  
+  for (int i = 1; i < sob.rows ; i++){
+    if (isExtrema(sob.at<double>(i,0), sob.at<double>(i-1,0))){
+      a0.push_back(i);
+    }
+  }  
+  Mat peaks(a0.size(), 1, CV_32S, a0.data());
+  vector<int> a1;
+  for (int i = 1; i < a0.size(); i++){
+    a1.push_back(a0[i] - a0[i-1]);
+  }
+  delta = Mat(a0.size()-1, 1, CV_32S, a1.data());
+  Mat mean, stddev;
+  meanStdDev(delta, mean, stddev);
+  double st = stddev.at<double>(0,0);
+  return st*st;
+}
+
+//TODO
+//implement
+void split_block(const vector<int>& pos, const vector<int>& neg){
+  sort(pos.begin(), pos.end());
+  sort(neg.begin(), neg.end());
+  maxp = pos[pos.size()-1];
+  maxn = neg[neg.size()-1];
+  medianPos = median(pos);
+  medianNeg = median(neg);
+  int splitPos;
+  if (maxp > medianPos)
+    ;
+  else if (maxn > medianNeg)
+    ;
+  else
+    ;//EXPLODE
+
+  
+  return;
+}
+
+//TODO
+//implement
+void recursive_filter(){
+  return;
+}
+
+vector<Rect> homogenous_regions(const Mat& img){
+  queue<Rect> q;
+  double varmax = 1.2;
+  q.push(Rect(0,0, img.cols, img.rows));
+  while(!q.empty()){
+    Rect r = q.front();
+    q.pop();
+    Mat block = img(r), grad, delta;    
+    if (homogenity_stats(block, grad, delta) > varmax)
+      split_block();
+  }
+
 }
 
 
@@ -80,7 +129,7 @@ Mat gray2binary(const Mat& gray){
   return bw;
 }
 
-vector<int> heuristic_filter(Mat& in, Mat& labels, Mat& stats ){
+vector<int> heuristic_filter(Mat& in, Mat& labels, Mat& stats, Mat& textimg, Mat& nontextimg){
   int nLabels = stats.rows;
   vector<Rect> bb(nLabels);
   bb[0] = Rect(-1, -1, 0, 0);
@@ -118,22 +167,28 @@ vector<int> heuristic_filter(Mat& in, Mat& labels, Mat& stats ){
   for (int i = 0; i <  nontext.size(); i++){
     mask = mask | (labels==nontext[i]);
   }
-  imshow("mask", mask);
-  waitKey(0);
-  Mat res(in.size(), CV_8UC1, Scalar(0));
-  in.copyTo(res, ~mask);
-  imshow("Res", res);
+  textimg = Mat(in.size(), CV_8UC1, Scalar(0));
+  in.copyTo(textimg, ~mask);
+  for (int i = 0; i <  text.size(); i++){
+    mask = mask | (labels== text[i]);
+  }
+  nontextimg = Mat(in.size(), CV_8UC1, Scalar(0));
+  in.copyTo(nontextimg, ~mask);
+  imshow("text", text);
   waitKey(0);
   return text;
 }
 
-void mla(){
-  /* vector<Rect> homboxes = homogenitySplit();
+void mla(const Mat& img){
+  vector<Rect> homboxes = homogenous_regions(img);
   for (int i = 0; i < homboxes.size(); i++){
-    recursive_filter(homboxes[i]);
-    }*/
+    //recursive_filter(homboxes[i]);
+  }
 }
 
+
+//BUG
+//invalid use of pointers. Needs fixing.
 vector<Rect> detect_tables(string filename){
   Mat img = imread(filename.c_str(), IMREAD_GRAYSCALE);  
   if (!img.data)
@@ -143,8 +198,9 @@ vector<Rect> detect_tables(string filename){
   Mat stats;
   Mat centroids;
   int nLabels = connectedComponentsWithStats(bw, cc, stats, centroids, 8, CV_32S);
-  heuristic_filter(bw, cc, stats);
-  //mla();
+  Mat text, nontext;
+  heuristic_filter(bw, cc, stats,text, nontext);
+  mla(text);
   //noise_removal(bw);
 }
 
--- test.cpp
+++ test.cpp
@@ -1,39 +1,75 @@
+#include <algorithm>
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <string>
 
 using namespace cv;
 using namespace std;
 
+bool isExtrema(double a, double b){
+  return (a < 0 && b > 0) || (a > 0 && b < 0);
+}
+
+double absol(double v){
+  if (v < 0) return -v;
+  return v;
+}
+
+
+void displayHist(const Mat& img, string str){
+
+  int m = 0;
+  for (int i = 0; i < img.rows; i++){
+    int val = img.at<double>(i,0);
+    m = max(m,val);
+  }
+  Mat hist = Mat::zeros(img.rows, m+50, CV_8U);
+  for (int i = 0; i < img.rows; i++){
+    for (int j = 0; j < img.at<double>(i,0); j++){
+      hist.at<uchar>(i,j) = 255;
+    }
+  }
+  imshow(str, hist);
+  waitKey(0);
+}
+
 int main(int argc, char** argv){
-  Mat rsz = imread(argv[1]);
-  if (!rsz.data)
-    cerr << "Problem loading image. " << endl;
-  Mat gray;
-  if (rsz.channels() == 3){
-    cvtColor(rsz, gray, CV_BGR2GRAY);
-  }else
-    gray = rsz;
+  Mat img = imread(argv[1], IMREAD_GRAYSCALE);
   Mat bw;
   adaptiveThreshold(~gray, bw, 255, CV_ADAPTIVE_THRESH_MEAN_C,THRESH_BINARY, 15, -2);
   Mat red;
   int s = 5;
-  reduce(bw, red, 1, CV_REDUCE_SUM, CV_32S);
-  Mat black = Mat::zeros(bw.size(), CV_8U);
-  Mat blurred;  
-  blur(red, blurred, Size(1,30));
-  Mat sob;
-  Sobel(blurred, sob, CV_32F ,CV_SCHARR, 0, 1);
-  Mat peaks = getPeaks(sob);
-  
-  for (int i = 0; i < dst.size().height; i++){
-    for (int j = 0; j < dst.at<int>(i,0)/255; j++){
-      black.at<uchar>(i,j) = 255;
-    }
-  }
-  imshow("img", black);  
-  waitKey(0);
+  //histogram
+  int scale = 255;
+  reduce(bw, red, 1, CV_REDUCE_SUM, CV_64F);
+  Mat sred = red / scale;
+  displayHist(sred, "red");
 
-  imshow("img", dst);
-  waitKey(0);
+  Mat blurred;
+  //medelkernel 
+  blur(sred, blurred, Size(1,30));
+  displayHist(blurred, "blurred");
+  Mat sob;
+  //gradienten
+  Scharr(blurred, sob, -1, 0, 1);
+  displayHist(sob, "sobel");
+  vector<int> a0;  
+  for (int i = 1; i < sob.rows ; i++){
+    if (isExtrema(sob.at<double>(i,0), sob.at<double>(i-1,0))){
+      a0.push_back(i);
+    }
+  }  
+  Mat peaks(a0.size(), 1, CV_32S, a0.data());
+  vector<int> a1;
+  for (int i = 1; i < a0.size(); i++){
+    a1.push_back(a0[i] - a0[i-1]);
+  }
+  Mat delta(a0.size()-1, 1, CV_32S, a1.data());
+  Mat mean, stddev;
+  meanStdDev(delta, mean, stddev);
+  //cout << "Mean: " << mean.at<double>(0,0) << " stddev: " << stddev.at<double>(0,0) << endl;
+  double st = stddev.at<double>(0,0);
+  double var = st*st;
+  cout << "Variance: " << var << endl;
   return 0;
 }

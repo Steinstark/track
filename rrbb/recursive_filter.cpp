12b6c46f1a13d74fdf86decf9258a5c4cdabf252
Henrik Josefsson <josefssonhenrik@hotmail.com>
2017-12-12 13:46:39 +0100

    replaced db with multiindex

--- rrbb/recursive_filter.cpp
+++ rrbb/recursive_filter.cpp
@@ -1,18 +1,38 @@
 #include <algorithm>
 #include <opencv2/opencv.hpp>
 #include <set>
+#include <iostream>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/identity.hpp>
+#include <boost/multi_index/member.hpp>
 #include "RTree.h"
 
 using namespace std;
 using namespace cv;
+using namespace boost::multi_index;
 
 struct AreaNode{
   int a, w, h, index;
   AreaNode(int a, int w, int h, int index): a(a), w(w), h(h), index(index){}
-  bool operator<(AreaNode other) const{
-    return a > other.a;
+  int operator[](size_t id){
+    if (id == 0) return a;
+    if (id == 1) return w;
+    if (id == 2) return h;
+    if (id == 3) return index;
+    return -1;
   }
 };
+
+using NodeDB = multi_index_container<
+  AreaNode,
+  indexed_by<
+    ordered_non_unique<member<AreaNode, int, &AreaNode::a>, greater<int> >,
+    ordered_non_unique<member<AreaNode, int, &AreaNode::w>, greater<int> >,
+    ordered_non_unique<member<AreaNode, int, &AreaNode::h>, greater<int> >,
+    ordered_unique<member<AreaNode, int, &AreaNode::index> >
+    >
+  >;
 
 bool callback(int id, void* arg){
   vector<int>* v = static_cast<vector<int>*>( arg );
@@ -32,22 +52,7 @@ int search_tree(RTree<int, int, 2, float>& tree, Rect r, vector<int>& vec){
   return tree.Search(tl, br, callback,(static_cast<void*>(&vec)));
 }
 
-double median(const vector<int>& v){
-  int n = v.size();
-  if (n % 2)
-    return v[n/2];
-  return (v[n/2-1] + v[n/2])/2;   
-}
-
-double mean(const vector<int>& v){
-  int m = 0; 
-  for (int i = 0; i < v.size(); i++){
-    m += v[i];
-  }
-  return m;
-}
-
-vector<int> minimum_median_filter(){
+vector<int> minimum_median_filter(NodeDB nodes){
   return vector<int>();
 }
 
@@ -59,65 +64,71 @@ vector<int> classify_elements(const vector<int>& se){
   return vector<int>();
 }
 
-vector<int> suspected_elements(const multiset<AreaNode>& area_set,
-			       const multiset<int>& width_set,
-			       const multiset<int>& height_set){
-  vector<int>  v;
-  while (){
-    auto it = area_set.begin();
-    int area = it->area;
-    int width = it->width;
-    int height = it->height;
-    int index = it->index;
-    int wm = width_set.begin();
-    int hm = height_set.begin();
-    double meana, meanw, meanh;
-    double meda, medw, medh;    
-    double k1 = k_calc(meana, meda);
-    double k2 = k_calc(meanw, medw);
-    double k3 = k_calc(meanh, medh);
-    if (a > k1*meda &&
-	((width == wm && width > k2*medw) ||
-	 (height == hm && height > k3*medh))){       
-      area_set.erase(it);
-      width_set.erase(width_set.find(width));
-      height_set.erase(height_set.find(height));
-      v.push_back(index);
-    }else
+bool isSuspected(NodeDB& nodes,
+			const vector<double>& means,
+			const vector<double>& medians){
+  int k[3];
+  for (int i = 0; i < 3; i++){
+    k[i] = k_calc(means[i], medians[i]);		    
+  }  
+  auto n1 = *nodes.get<0>().begin();
+  if (n1.a > k[0]*medians[0]){
+    auto n2 = *nodes.get<1>().begin();      
+    if (n1.index  == n2.index && n2.w > k[1]*medians[1])
+      return true;
+    n2 = *nodes.get<2>().begin();
+    if (n1.index == n2.index && n2.h > k[2]*medians[2])
+      return true;
+  }    
+  return false;
+}
+
+//TODO
+//implement median for a NodeDB
+vector<double> getMedians(NodeDB nodes){
+  vector<double>();
+}
+
+//TODO
+//implement mean for a NodeDB
+vector<double> getMeans(NodeDB nodes){
+  return vector<double>();
+}
+
+vector<int> suspected_elements(NodeDB& nodes){
+  vector<int> v;
+  while (1){
+    vector<double> medians = getMedians(nodes);
+    vector<double> means = getMeans(nodes);
+    bool suspect = isSuspected(nodes, means, medians);
+    if (!suspect)
       break;
+    auto& p = nodes.get<0>();
+    v.push_back(p.begin()->index);
+    p.erase(p.begin());
   }
   return v;
 }
 
-void recursive_filter(const Mat& img){
-  const int inf = 1000000;
-  Mat cc, stats, centroids;
-  int labels = connectedComponentsWithStats(img, cc, stats, centroids, 8, CV_32S);
+vector<int> recursive_filter(const Mat& img, const Mat& stats){
+  int labels = stats.rows;
   RTree<int, int, 2, float> tree;
   vector<Rect> bb(labels);
-  vector<int> areas(labels);
+  NodeDB nodes;
   for (int i = 1; i < labels; i++){
     int left = stats.at<int>(i, CC_STAT_LEFT);
     int top = stats.at<int>(i, CC_STAT_TOP);
     int width = stats.at<int>(i, CC_STAT_WIDTH);
     int height = stats.at<int>(i, CC_STAT_HEIGHT);
-       bb[i] = Rect(left, top, width, height);
-    areas[i] = stats.at<int>(i, CC_STAT_AREA);
+    int area = stats.at<int>(i, CC_STAT_AREA);
+    bb[i] = Rect(left, top, width, height);
+    nodes.insert({area, width, height, i});
     insert2tree(tree, bb[i], i);
   }
-  multiset<AreaNode> area_set;
-  multiset<int> width_set;
-  multiset<int> height_set;
-  for (int i = 0; i < bb.size(); i++){
-    area_set.insert(AreaNode(areas[i], bb[i].width, bb[i].height, i));
-    width_set.insert(bb[i].width);
-    height_set.insert(bb[i].height);    
-  }
-  vector<int> se = suspected_elements(area_set, width_set, height_set);
+  vector<int> se = suspected_elements(nodes);
   vector<int> ce = classify_elements(se);
-  vector<int> me = minimum_median_filter();
-
- 
+  vector<int> me = minimum_median_filter(nodes);
+  
   /*  for (int i = 1; i < labels; i++){
     vector<int> ln;
     Rect lr(-inf, bb[i].y, bb[i].x, bb[i].height);
@@ -130,7 +141,7 @@ void recursive_filter(const Mat& img){
     }*/
   
 }
-
+ 
 
 int main(){
   

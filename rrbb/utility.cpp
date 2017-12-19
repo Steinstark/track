cf132ce9c5bf7ae48ae05532842aa48bbba80eba
Henrik Josefsson <josefssonhenrik@hotmail.com>
2017-12-19 16:11:12 +0100

    added utility

---
+++ rrbb/utility.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+#include "utility.hpp"
+
+void move2(Mat& from, Mat& to, const Mat& cc,  Rect r , int i){
+  Mat mask(cc.size(), CV_8UC1, Scalar(0));
+  mask = mask | (cc==i);
+  mask = mask & from;
+  to = to | mask;
+  from = from & ~mask;
+}
---
+++ rrbb/utility.hpp
@@ -0,0 +1,8 @@
+#ifndef TRACK_UTILITY_HPP
+#define TRACK_UTILITY_HPP
+
+#include <opencv2/opencv.hpp>
+
+void move2(cv::Mat& from, cv::Mat& to, const cv::Mat& cc,  cv::Rect r , int i);
+
+#endif

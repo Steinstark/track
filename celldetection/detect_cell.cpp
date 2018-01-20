47f8e4bbbe9416a62c563ef9a8e91060aad22ec0
Henrik Josefsson <josefssonhenrik@hotmail.com>
2018-01-20 12:14:44 +0100

    minor coderestructuring

--- celldetection/detect_cell.cpp
+++ celldetection/detect_cell.cpp
@@ -1,20 +1,17 @@
-#include <iostream>
+#include <vector>
 #include <opencv2/opencv.hpp>
-#include <string>
 #include "detect_cell.hpp"
 
 using namespace std;
 using namespace cv;
 
-std::vector<Rect> detect_cells(const Mat& bw, Rect r){
+//IMPROVEMENT
+//remove magic number for size of dilate
+vector<Rect> detect_cells(const Mat& bw, Rect r){
   Mat tableImage = bw(r);
-  //IMPROVEMENT
-  //remove magic number
   Mat mask;
   Mat element = getStructuringElement(MORPH_RECT, Size(20,5));
-  dilate(tableImage, mask, element, Point(-1,-1));
-  //erode(mask, mask, element, Point(-1,-1));
-  
+  dilate(tableImage, mask, element, Point(-1,-1));  
   //imshow("img", mask);
   //waitKey(0);  
   vector<vector<Point> > contours;
@@ -22,17 +19,16 @@ std::vector<Rect> detect_cells(const Mat& bw, Rect r){
   findContours(mask, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0,0));
   vector<vector<Point> > contours_poly(contours.size());
   vector<Rect> boundRect( contours.size());
-
   for (size_t i = 0; i < contours.size(); i++){
     approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true);
     boundRect[i] = boundingRect( Mat(contours_poly[i])) + r.tl();
   }
-  Mat drawing = bw.clone();
+  /*Mat drawing = bw.clone();
   Scalar color = Scalar(255);
   for (size_t i = 0; i < contours.size(); i++){
     //    drawContours(drawing,contours_poly, (int)i, color, 1,8, vector<Vec4i>(), 0, Point() );
     rectangle(drawing, boundRect[i], color);
-  }
+    }*/
   //imshow("img", drawing);
   //  waitKey(0);
   return boundRect;
--- celldetection/detect_cell.hpp
+++ celldetection/detect_cell.hpp
@@ -1,10 +1,8 @@
 #ifndef TRACK_DETECT_CELL
 #define TRACK_DETECT_CELL
 
-#include <iostream>
 #include <opencv2/opencv.hpp>
 #include <vector>
-#include <string>
 
 std::vector<cv::Rect> detect_cells(const cv::Mat& bw, cv::Rect r);
 #endif

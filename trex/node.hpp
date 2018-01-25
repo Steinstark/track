3f1e22267e266a561e58e9eb10d17b45b63cde22
Henrik Josefsson <josefssonhenrik@hotmail.com>
2018-01-25 14:12:41 +0100

    added NodeElement

---
+++ trex/node.hpp
@@ -0,0 +1,9 @@
+#ifndef TRACK_NODEELEMENT_HPP
+#define TRACK_NODEELEMENT_HPP
+
+struct NodeElement{
+  int a, b, index;
+  NodeElement(int a, int b, int index): a(a), b(b), index(index){}
+};
+
+#endif

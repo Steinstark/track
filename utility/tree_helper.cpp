842423561fad2a7d7a67aef80a92c56070249020
Henrik Josefsson <josefssonhenrik@hotmail.com>
2018-02-12 16:11:18 +0100

    added nn search

--- utility/tree_helper.cpp
+++ utility/tree_helper.cpp
@@ -16,15 +16,32 @@ value cvr2v(const cv::Rect& r, int index){
   return value(cvr2bb(r), index);
 }
 
+cv::Point bp2cvp(point p){
+  return cv::Point(p.get<0>(), p.get<1>());
+}
+
+cv::Rect bb2cvr(box b){
+  return cv::Rect(bp2cvp(b.min_corner()), bp2cvp(b.max_corner()));
+}
+
 void tree::insert2tree(RT& tree, const cv::Rect& r, int index){
   tree.insert(cvr2v(r, index));
+}
+
+std::vector<cv::Rect> tree::closestBox(RT& tree, const cv::Point cvp, const cv::Rect& r){
+  point p = cvp2bp(cvp);
+  box b = cvr2bb(r);
+  std::vector<value> boxes;
+  tree.query(bgi::nearest(p, 1) && bgi::intersects(b), std::back_inserter(boxes));
+  std::vector<cv::Rect> rects;
+  std::transform(boxes.begin(), boxes.end(), std::back_inserter(rects),[](value v){return bb2cvr(v.first);});
 }
 
 std::vector<int> tree::search_tree(RT& tree, const cv::Rect& r){
   box query_box = cvr2bb(r);
   std::vector<int> index;
   std::vector<value> values;
-  tree.query(boost::geometry::index::intersects(query_box), std::back_inserter(values));
+  tree.query(bgi::intersects(query_box), std::back_inserter(values));
   std::transform(values.begin(), values.end(), std::back_inserter(index),[](value v){return v.second;});
   return index;
 }
--- utility/tree_helper.hpp
+++ utility/tree_helper.hpp
@@ -13,9 +13,11 @@ namespace tree{
     using point = boost::geometry::model::point<int, 2, boost::geometry::cs::cartesian>;
     using box = boost::geometry::model::box<point>;
     using value = std::pair<box, int>;
+    namespace bgi = boost::geometry::index;
   }
-  using RT = boost::geometry::index::rtree<internal::value, boost::geometry::index::quadratic<16> >;
+  using RT = boost::geometry::index::rtree<internal::value, boost::geometry::index::quadratic<16> >;  
   void insert2tree(RT& tree, const cv::Rect& r, int index);
+  std::vector<cv::Rect> closestBox(RT& tree, const cv::Point cvp, const cv::Rect& r);
   std::vector<int> search_tree(RT& tree, const cv::Rect& r);
   void remove_tree(RT& tree, const cv::Rect& r, int index);
 }

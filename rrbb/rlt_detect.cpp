cb9c2a229ae8bcdfda5f9e8f18b33ea73e95c366
Henrik Josefsson <josefssonhenrik@hotmail.com>
2018-02-15 17:07:54 +0100

    changed vector to list

--- rrbb/rlt_detect.cpp
+++ rrbb/rlt_detect.cpp
@@ -1,4 +1,5 @@
 #include "rlt_detect.hpp"
+#include <list>
 #include <vector>
 #include <opencv2/opencv.hpp>
 #include "utility.hpp"
@@ -38,24 +39,24 @@ bool candidateRLT(ImageMeta& im, ComponentStats& cs){
      isColorBlock(im, cs));
 }
 
-vector<Rect> findRLT(ImageDataBox& imd, ImageMeta& im)
+list<Rect> findRLT(ImageDataBox& imd, ImageMeta& im)
 {
-  vector<int> candidates;
+  list<ComponentStats> candidates;
   vector<ComponentStats>& nontextData = imd.nontextData;
   vector<ComponentStats>& textData = imd.textData;
   Mat& text = imd.text;
-  for (int i = 0; i < nontextData.size(); i++){
-    if (candidateRLT(im, nontextData[i])){
-      candidates.push_back(i);
+  for (ComponentStats& cs: nontextData){
+    if (candidateRLT(im, cs)){
+      candidates.push_back(cs);
     }
   }
-  vector<Rect> verified;
-  for (int i = 0; i < candidates.size(); i++){
-    Mat skewedRegion = text(nontextData[candidates[i]].r), correctedRegion;
+  list<Rect> verified;
+  for (auto it = candidates.begin(); it != candidates.end(); it++){
+    Mat skewedRegion = text(it->r), correctedRegion;
     double angle = counterRotAngle(skewedRegion);
     rotate(skewedRegion, correctedRegion, angle);
-    if (verifyRLT(correctedRegion, im, textData, nontextData[candidates[i]])){
-      verified.push_back(nontextData[candidates[i]].r);
+    if (verifyRLT(correctedRegion, im, textData, *it)){
+      verified.push_back(it->r);
     }
   }
   return verified;

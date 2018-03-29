5af988c938600c7b10b1aff782f31063e118abf6
Henrik Josefsson <josefssonhenrik@hotmail.com>
2018-03-29 16:24:10 +0200

    added debug

---
+++ rrbb/debug_header.cpp
@@ -0,0 +1,8 @@
+#include "debug_header.hpp"
+
+using namespace cv;
+
+Mat textDebug;
+Mat nontextDebug;
+Mat lineDebug;
+Mat expandedDebug;
---
+++ rrbb/debug_header.hpp
@@ -0,0 +1,12 @@
+#include <opencv2/opencv.hpp>
+
+#ifndef TRACK_DEBUG_HEADER_HPP
+#define TRACK_DEBUG_HEADER_HPP
+
+//DEBUG
+extern cv::Mat textDebug;
+extern cv::Mat nontextDebug;
+extern cv::Mat lineDebug;
+extern cv::Mat expandedDebug;
+
+#endif

5f7feb1eabb072e2ddb22653d02ccf15be3f50cd
Henrik Josefsson <josefssonhenrik@hotmail.com>
2018-04-16 10:24:00 +0200

    changed params and removed unused header

--- rrbb/heuristic_filter.cpp
+++ rrbb/heuristic_filter.cpp
@@ -1,8 +1,6 @@
 #include "heuristic_filter.hpp"
 #include <vector>
 #include <boost/geometry.hpp>
-#include <boost/geometry/geometries/register/point.hpp>
-#include <boost/geometry/geometries/register/box.hpp>
 #include "utility.hpp"
 #include "component_stats.hpp"
 #include "image_util.hpp"
@@ -19,8 +17,8 @@ void heuristic_filter(Mat& text, Mat& nontext){
   for (ComponentStats& cs : stats){
     if (cs.area < 6 ||
 	(cs.hwratio < 0.15 && cs.r.width > cs.r.height) ||      
-	cs.density < 0.15 ||
-	distance(tree.qbegin(bgi::intersects(cs)), tree.qend()) > 5){
+	cs.density < 0.25 ||
+	distance(tree.qbegin(bgi::intersects(cs)), tree.qend()) > 4){
       move2(text, nontext, cc, cs.index);
     }    
   }  

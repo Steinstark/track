6cd3243633b06d36f3b1e9b81831a7ed404d455e
Henrik Josefsson <josefssonhenrik@hotmail.com>
2018-04-18 11:50:58 +0200

    minor cleanup

--- rrbb/heuristic_filter.cpp
+++ rrbb/heuristic_filter.cpp
@@ -13,13 +13,13 @@ void heuristic_filter(Mat& text, Mat& nontext){
   remove_lines(text, nontext);
   Mat cc;
   vector<ComponentStats> stats = statistics(text, cc);
-  bgi::rtree<ComponentStats, bgi::quadratic<16> > tree(stats);						       
+  bgi::rtree<ComponentStats, bgi::quadratic<16> > tree(stats);
   for (ComponentStats& cs : stats){
     if (cs.area < 6 ||
-	(cs.hwratio < 0.15 && cs.r.width > cs.r.height) ||      
+	(cs.hwratio < 0.15 && cs.r.width > cs.r.height) ||
 	cs.density < 0.25 ||
 	distance(tree.qbegin(bgi::intersects(cs)), tree.qend()) > 4){
       move2(text, nontext, cc, cs.index);
-    }    
-  }  
+    }
+  }
 }

5171f8963034dc71138dc354fe4dbb64e823d309
Henrik Josefsson <josefssonhenrik@hotmail.com>
2018-04-26 15:09:56 +0200

    removed old files

--- rrbb/region_refinement.cpp
+++
@@ -1,6 +0,0 @@
-using namespace std;
-using namespace cv;
-
-void refine_regions(Mat& text, Mat& lines, Mat& tables, Mat& seps, Mat& images){
-  
-}
--- rrbb/segment.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef TRACK_SEGMENT_HPP
-#define TRACK_SEGMENT_HPP
-
-#include <opencv2/opencv.hpp>
-
-cv::Mat segment(cv::Mat& text, cv::Mat& nontext);
-
-#endif

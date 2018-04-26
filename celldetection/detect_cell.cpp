4bcf6114cfaee39e4552dad2fd0c0d06046616d3
Henrik Josefsson <josefssonhenrik@hotmail.com>
2018-04-26 18:03:25 +0200

    removed comments

--- celldetection/detect_cell.cpp
+++ celldetection/detect_cell.cpp
@@ -12,8 +12,6 @@ vector<Rect> detect_cells(const Mat& bw, Rect r){
   Mat mask;
   Mat element = getStructuringElement(MORPH_RECT, Size(20,5));
   dilate(tableImage, mask, element, Point(-1,-1));  
-  //imshow("img", mask);
-  //waitKey(0);  
   vector<vector<Point> > contours;
   vector<Vec4i> hierarchy;
   findContours(mask, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0,0));
@@ -23,13 +21,5 @@ vector<Rect> detect_cells(const Mat& bw, Rect r){
     approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true);
     boundRect[i] = boundingRect( Mat(contours_poly[i])) + r.tl();
   }
-  /*Mat drawing = bw.clone();
-  Scalar color = Scalar(255);
-  for (size_t i = 0; i < contours.size(); i++){
-    //    drawContours(drawing,contours_poly, (int)i, color, 1,8, vector<Vec4i>(), 0, Point() );
-    rectangle(drawing, boundRect[i], color);
-    }*/
-  //imshow("img", drawing);
-  //  waitKey(0);
   return boundRect;
 }
